89428d3f9111dc95731ec7f6bc1b38d24e5798c7
Carlos Zapata <cdanielzt@gmail.com>
2023-03-29 13:54:00 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+
+    plugin(function({ addComponents }) {
+      const buttons = {
+        '.btn': {
+          padding: '.5rem 1rem !important',
+          borderRadius: '.25rem !important',
+          fontWeight: '600 !important',
+          background: 'red'
+        },
+      }
+
+      addComponents(buttons)
+    })
+
+  ]
+
+
+}
